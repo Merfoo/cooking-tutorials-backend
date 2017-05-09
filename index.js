9d6f2e68d97db8983e69a35149069c9b23fc9384
Fauzi Kliman <fauzimerfoo@gmail.com>
2017-05-09 04:19:00 -0700

    Basic server setup to respond true/false success to contact endpoint

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var express = require("express")
+var app = express();
+var bodyParser = require("body-parser");
+var contact = require("./routes/contact");
+
+app.use("/", express.static("../cooking-tutorials/dist"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/contact", contact);
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
+});
---
+++ routes/contact.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var request = require("request");
+var router = express.Router();
+
+var headers = {
+  "User-Agent": "Super Agent/0.0.1",
+  "Content-Type": "application/x-www-forum-urlencoded"
+};
+
+router.post("/", (req, res) => {
+  request({
+    url: "https://www.google.com/recaptcha/api/siteverify",
+    method: "POST",
+    headers: headers,
+    json: true,
+    form: { 
+      "secret": process.env.RECAPTCHA_SECRET_KEY,
+      "response": req.body["g-recaptcha-response"]
+    }
+  }, (error, response, body) => {
+    if (body.success) {
+      res.json({ "success": true });
+    }
+
+    else {
+      res.json({ "success": false });
+    }
+  });
+});
+
+module.exports = router;
