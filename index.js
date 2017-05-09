6a73ea6f9505833327ea96eaffe75c005a082302
Fauzi Kliman <fauzimerfoo@gmail.com>
2017-05-09 04:36:26 -0700

    Added public files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var contact = require("./routes/contact");
 
-app.use("/", express.static("../cooking-tutorials/dist"));
+app.use(express.static("./public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/contact", contact);
