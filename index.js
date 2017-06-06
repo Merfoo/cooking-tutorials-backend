38181cf546bccab8a65a69970a9d9907848e4bd0
Fauzi Kliman <fauzimerfoo@gmail.com>
2017-06-06 13:51:51 -0700

    Added support to use history mode for urls instead of #

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 var express = require("express")
+var history = require("connect-history-api-fallback");
 var app = express();
 var bodyParser = require("body-parser");
 var contact = require("./routes/contact");
 var donate = require("./routes/donate");
 
+app.use(history());
 app.use("/", express.static("../cooking-tutorials/dist"));
 app.use(express.static("./public"));
 app.use(bodyParser.json());
